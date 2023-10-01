ca9cb3bcef2d8b8c17d53a96fc14e7329c5cd758
Vinícius Stringue de Paula <111091227+vinistringue@users.noreply.github.com>
2023-10-01 17:06:38 -0300

    Commit #5: Melhorando o projeto

--- TaskMinder/assets/js/app.js
+++ TaskMinder/assets/js/app.js
@@ -7,15 +7,13 @@ app.use(express.json());
 // Lista de tarefas (exemplo)
 const tarefas = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3'];
 
-// Rotas do servidor Express
-
-// Rota para retornar a lista de tarefas
-app.get('/tarefas', (req, res) => {
+// Função para obter a lista de tarefas
+function obterTarefas(req, res) {
     res.json({ tarefas });
-});
+}
 
-// Rota para adicionar uma nova tarefa à lista
-app.post('/tarefas', (req, res) => {
+// Função para adicionar uma nova tarefa à lista
+function adicionarTarefa(req, res) {
     const novaTarefa = req.body.tarefa;
     if (novaTarefa) {
         tarefas.push(novaTarefa);
@@ -23,10 +21,10 @@ app.post('/tarefas', (req, res) => {
     } else {
         res.status(400).json({ erro: 'A tarefa não pode estar em branco.' });
     }
-});
+}
 
-// Rota para editar uma tarefa
-app.put('/tarefas/:id', (req, res) => {
+// Função para editar uma tarefa
+function editarTarefa(req, res) {
     const id = req.params.id;
     const novoNome = req.body.novoNome;
     if (id >= 0 && id < tarefas.length && novoNome) {
@@ -35,10 +33,10 @@ app.put('/tarefas/:id', (req, res) => {
     } else {
         res.status(400).json({ erro: 'ID inválido ou novo nome em branco.' });
     }
-});
+}
 
-// Rota para excluir uma tarefa
-app.delete('/tarefas/:id', (req, res) => {
+// Função para excluir uma tarefa
+function excluirTarefa(req, res) {
     const id = req.params.id;
     if (id >= 0 && id < tarefas.length) {
         const tarefaRemovida = tarefas.splice(id, 1);
@@ -46,9 +44,49 @@ app.delete('/tarefas/:id', (req, res) => {
     } else {
         res.status(400).json({ erro: 'ID inválido.' });
     }
-});
+}
+
+// Definindo as rotas
+
+// Rota para retornar a lista de tarefas
+app.get('/tarefas', obterTarefas);
+
+// Rota para adicionar uma nova tarefa à lista
+app.post('/tarefas', adicionarTarefa);
+
+// Rota para editar uma tarefa
+app.put('/tarefas/:id', editarTarefa);
+
+// Rota para excluir uma tarefa
+app.delete('/tarefas/:id', excluirTarefa);
 
 // Iniciar o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
+});
+
+// Evento para adicionar tarefa quando a tecla Enter for pressionada
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const novaTarefa = document.getElementById('novaTarefa').value;
+        if (novaTarefa.trim() !== '') {
+            // Realize uma solicitação HTTP POST para adicionar a tarefa
+            fetch('/tarefas', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ tarefa: novaTarefa }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    alert(data.mensagem);
+                    document.getElementById('novaTarefa').value = '';
+                    // Atualize a lista de tarefas na interface do usuário aqui, se necessário
+                })
+                .catch((error) => {
+                    console.error('Erro ao adicionar tarefa:', error);
+                });
+        }
+    }
 });
