9ce40ccb819b7bd9036b9e35bd7a32ed49170cbd
Vinícius Stringue de Paula <111091227+vinistringue@users.noreply.github.com>
2023-10-01 16:27:53 -0300

    Commit #4: Adicionando Rodapé

--- TaskMinder/assets/js/app.js
+++ TaskMinder/assets/js/app.js
@@ -4,185 +4,51 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// Lista de tarefas (exemplo)
+const tarefas = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3'];
+
 // Rotas do servidor Express
 
-// Rota para retornar a lista de tarefas (exemplo)
+// Rota para retornar a lista de tarefas
 app.get('/tarefas', (req, res) => {
-    const tarefas = ['Tarefa 1', 'Tarefa 2', 'Tarefa 3'];
     res.json({ tarefas });
 });
 
-// Rota para adicionar uma nova tarefa à lista (exemplo)
+// Rota para adicionar uma nova tarefa à lista
 app.post('/tarefas', (req, res) => {
     const novaTarefa = req.body.tarefa;
-    // Lógica para adicionar a nova tarefa à lista (você pode implementar aqui)
-    res.json({ mensagem: 'Tarefa adicionada com sucesso!' });
+    if (novaTarefa) {
+        tarefas.push(novaTarefa);
+        res.json({ mensagem: 'Tarefa adicionada com sucesso!' });
+    } else {
+        res.status(400).json({ erro: 'A tarefa não pode estar em branco.' });
+    }
 });
 
-// Rota para editar uma tarefa (exemplo)
+// Rota para editar uma tarefa
 app.put('/tarefas/:id', (req, res) => {
     const id = req.params.id;
     const novoNome = req.body.novoNome;
-    // Lógica para editar a tarefa com o ID especificado (você pode implementar aqui)
-    res.json({ mensagem: 'Tarefa editada com sucesso!' });
+    if (id >= 0 && id < tarefas.length && novoNome) {
+        tarefas[id] = novoNome;
+        res.json({ mensagem: 'Tarefa editada com sucesso!' });
+    } else {
+        res.status(400).json({ erro: 'ID inválido ou novo nome em branco.' });
+    }
 });
 
-// Rota para excluir uma tarefa (exemplo)
+// Rota para excluir uma tarefa
 app.delete('/tarefas/:id', (req, res) => {
     const id = req.params.id;
-    // Lógica para excluir a tarefa com o ID especificado (você pode implementar aqui)
-    res.json({ mensagem: 'Tarefa excluída com sucesso!' });
+    if (id >= 0 && id < tarefas.length) {
+        const tarefaRemovida = tarefas.splice(id, 1);
+        res.json({ mensagem: `Tarefa "${tarefaRemovida}" excluída com sucesso!` });
+    } else {
+        res.status(400).json({ erro: 'ID inválido.' });
+    }
 });
 
 // Iniciar o servidor
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
-});
-
-// Função para adicionar uma tarefa à lista
-function adicionarTarefa(tarefa) {
-    const li = document.createElement("li");
-    li.textContent = tarefa;
-
-    const divBotoes = criarBotoesAcao();
-    li.appendChild(divBotoes);
-
-    listaTarefas.appendChild(li);
-}
-
-// Função para criar um botão de ação para uma tarefa
-function criarBotoesAcao() {
-    const divBotoes = document.createElement("div");
-    divBotoes.className = "botoes-acao";
-
-    const editarBtn = document.createElement("button");
-    editarBtn.textContent = "Editar";
-    editarBtn.className = "btn btn-info btn-sm";
-    editarBtn.addEventListener("click", editarTarefa);
-
-    const concluirBtn = document.createElement("button");
-    concluirBtn.textContent = "Concluir";
-    concluirBtn.className = "btn btn-success btn-sm";
-    concluirBtn.addEventListener("click", concluirTarefa);
-
-    const excluirBtn = document.createElement("button");
-    excluirBtn.textContent = "Excluir";
-    excluirBtn.className = "btn btn-danger btn-sm";
-    excluirBtn.addEventListener("click", excluirTarefa);
-
-    divBotoes.appendChild(editarBtn);
-    divBotoes.appendChild(concluirBtn);
-    divBotoes.appendChild(excluirBtn);
-
-    return divBotoes;
-}
-
-// Função para editar uma tarefa
-function editarTarefa() {
-    const novoNome = prompt("Editar a tarefa:", this.parentElement.parentElement.firstChild.textContent);
-    if (novoNome !== null) {
-        this.parentElement.parentElement.firstChild.textContent = novoNome;
-    }
-}
-
-// Função para marcar uma tarefa como concluída
-function concluirTarefa() {
-    this.parentElement.parentElement.firstChild.style.textDecoration = "line-through";
-}
-
-// Função para excluir uma tarefa
-function excluirTarefa() {
-    this.parentElement.parentElement.remove();
-}
-
-// Função para adicionar uma tarefa à lista
-function adicionarTarefa(tarefa, listaId) {
-    const li = document.createElement("li");
-    li.textContent = tarefa;
-
-    const divBotoes = criarBotoesAcao();
-    li.appendChild(divBotoes);
-
-    const lista = document.getElementById(listaId);
-    lista.appendChild(li);
-
-    // Limpar o campo de texto após adicionar a tarefa
-    document.getElementById("novaTarefa").value = "";
-}
-
-// Função para criar um botão de ação para uma tarefa
-function criarBotoesAcao() {
-    const divBotoes = document.createElement("div");
-    divBotoes.className = "botoes-acao";
-
-    const editarBtn = document.createElement("button");
-    editarBtn.textContent = "Editar";
-    editarBtn.className = "btn btn-info btn-sm";
-    editarBtn.addEventListener("click", editarTarefa);
-
-    const concluirBtn = document.createElement("button");
-    concluirBtn.textContent = "Concluir";
-    concluirBtn.className = "btn btn-success btn-sm";
-    concluirBtn.addEventListener("click", concluirTarefa);
-
-    const excluirBtn = document.createElement("button");
-    excluirBtn.textContent = "Excluir";
-    excluirBtn.className = "btn btn-danger btn-sm";
-    excluirBtn.addEventListener("click", excluirTarefa);
-
-    divBotoes.appendChild(editarBtn);
-    divBotoes.appendChild(concluirBtn);
-    divBotoes.appendChild(excluirBtn);
-
-    return divBotoes;
-}
-
-// Função para editar uma tarefa
-function editarTarefa() {
-    const novoNome = prompt("Editar a tarefa:", this.parentElement.parentElement.firstChild.textContent);
-    if (novoNome !== null) {
-        this.parentElement.parentElement.firstChild.textContent = novoNome;
-    }
-}
-
-// Função para marcar uma tarefa como concluída
-function concluirTarefa() {
-    this.parentElement.parentElement.firstChild.style.textDecoration = "line-through";
-}
-
-// Função para excluir uma tarefa
-function excluirTarefa() {
-    this.parentElement.parentElement.remove();
-}
-
-// Adicionar uma nova tarefa ao pressionar Enter no campo de texto
-document.getElementById("novaTarefa").addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        const novaTarefa = this.value.trim();
-        if (novaTarefa !== "") {
-            adicionarTarefa(novaTarefa, "afazer");
-        }
-    }
-});
-
-// Inicializar funcionalidade de arrastar e soltar usando a biblioteca Sortable
-const sortableLists = new Sortable(document.querySelectorAll(".list-group"), {
-    animation: 150,
-    onEnd: function (evt) {
-        const item = evt.item;
-        const tarefa = item.textContent.trim();
-        const listaDestinoId = item.parentElement.id;
-
-        // Verifique em qual lista a tarefa foi solta e atualize-a
-        if (listaDestinoId === "afazer") {
-            adicionarTarefa(tarefa, "afazer");
-        } else if (listaDestinoId === "emandamento") {
-            adicionarTarefa(tarefa, "emandamento");
-        } else if (listaDestinoId === "concluidas") {
-            adicionarTarefa(tarefa, "concluidas");
-        }
-
-        // Remova o item original (a tarefa arrastada) da lista de origem
-        item.remove();
-    },
 });
